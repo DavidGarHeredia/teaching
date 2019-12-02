4dde1d51501d5aead471fee6a68b02e62ac94c3d
David García Heredia <dgheredi@est-econ.uc3m.es>
2019-12-02 19:07:02 +0100

    Update code.R

--- ggplot2/code.R
+++ ggplot2/code.R
@@ -217,3 +217,4 @@ multiplePlots2 <- plot_grid(plotHist2,
 
 plot_grid(title, multiplePlots2, ncol = 1, rel_heights = c(0.1, 1)) 
 
+# https://drive.google.com/drive/folders/1JquAL_WJprY_nkkXyFKD-nb_0-IDZpij?usp=sharing
